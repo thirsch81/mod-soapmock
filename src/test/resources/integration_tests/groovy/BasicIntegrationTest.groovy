2100da36ad8b28be129b7497dfe857596f2dc9c2
Thomas Hirsch <thirsch@gmx.net>
2013-06-11 02:07:02 +0200

    class cast error

--- src/test/resources/integration_tests/groovy/BasicIntegrationTest.groovy
+++ src/test/resources/integration_tests/groovy/BasicIntegrationTest.groovy
@@ -1,8 +1,10 @@
 import static org.vertx.testtools.VertxAssert.*
 
 import org.vertx.groovy.testtools.VertxTests
+
 import de.thhi.soapmock.MockServerVerticle
 import de.thhi.soapmock.RenderVerticle
+
 
 // The test methods must being with "test"
 
@@ -11,27 +13,18 @@ def testHTTP() {
 	container.deployVerticle("groovy:" + MockServerVerticle.class.name) { result ->
 		assertNotNull(result)
 		container.logger.info(result?.cause())
-		container.logger.info(result.toString())
-		assertTrue(result.succeeded())
+		assertTrue(result.succeeded)
+		testComplete()
 	}
 
 	//    vertx.createHttpClient().setPort(8181).getNow("/", { resp ->
 	//      assertEquals(200, resp.statusCode)
-	/*
-	 If we get here, the test is complete
-	 You must always call `testComplete()` at the end. Remember that testing is *asynchronous* so
-	 we cannot assume the test is complete by the time the test method has finished executing like
-	 in standard synchronous tests
-	 */
-	testComplete()
-	//})
-
 }
 
 def testDeployMockServerVerticle() {
 	container.deployVerticle("groovy:" + MockServerVerticle.class.name) { result ->
 		container.logger.info("Error: " + result?.cause())
-		assertTrue(result.succeeded())
+		assertTrue(result.succeeded)
 		testComplete()
 	}
 }
@@ -39,14 +32,14 @@ def testDeployMockServerVerticle() {
 def testDeployRenderVerticle() {
 	container.deployWorkerVerticle("groovy:" + RenderVerticle.class.name) { result ->
 		container.logger.info("Error: " + result?.cause())
-		assertTrue(result.succeeded())
+		assertTrue(result.succeeded)
 		testComplete()
 	}
 }
 
 def testRenderVerticle() {
 	container.deployWorkerVerticle("groovy:" + RenderVerticle.class.name) { result ->
-		assertTrue(result.succeeded())
+		assertTrue(result.succeeded)
 		vertx.eventBus.send("render", ["name" : "response", "binding" : ["content" : "some content"]]) { reply ->
 			assertNotNull(reply)
 			container.logger.info(reply.body)
