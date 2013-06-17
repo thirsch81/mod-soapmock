4ef42f06808de10b30e21835ff3c7b46b84ba95f
Thomas Hirsch <thirsch@gmx.net>
2013-06-17 23:39:30 +0200

    fiddling with angular

--- src/main/resources/web/js/templates.js
+++ src/main/resources/web/js/templates.js
@@ -1,18 +1,18 @@
 jQuery(function() {
-	$(document).on("click", ".template-delete", function() {
-		var templateName = $(this).attr("id").replace("-template-delete", "");
-		removeTemplate(templateName);
-	});
-	$(document).on("click", ".template-submit", function() {
-		var templateName = $(this).attr("id").replace("-template-submit", "");
-		submitTemplate(templateName);
-	});
-	$("a[href='#templates']").click(function() {
-		fetchTemplates();
-	});
-	$("#add-template").click(function() {
-		addTemplate();
-	});
+	// $(document).on("click", ".template-delete", function() {
+	// var templateName = $(this).attr("id").replace("-template-delete", "");
+	// removeTemplate(templateName);
+	// });
+	// $(document).on("click", ".template-submit", function() {
+	// var templateName = $(this).attr("id").replace("-template-submit", "");
+	// submitTemplate(templateName);
+	// });
+	// $("a[href='#templates']").click(function() {
+	// fetchTemplates();
+	// });
+	// $("#add-template").click(function() {
+	// addTemplate();
+	// });
 });
 
 function fetchTemplates() {
@@ -34,7 +34,9 @@ function updateTemplates(reply) {
 	if (reply.status == "ok") {
 		clearTemplates();
 		insertTemplates(reply.templates);
-		setTimeout(function() { $("#template-tabs a:first").click(); });
+		setTimeout(function() {
+			$("#template-tabs a:first").click();
+		});
 	}
 }
 
@@ -71,19 +73,32 @@ function clearTemplates() {
 	$("#template-container").empty();
 }
 
-function templateTab(name) {
-	return '<li><a href="#' + name + '" data-toggle="tab">' + name + '</a></li>';
-}
+function TemplateCtrl($scope) {
+	$scope.templates = [{
+		name : "test",
+		template : "test-template",
+	}, {
+		name : "name2",
+		template : "test",
+	}];
+	$scope.activeTemplate = "";
+	$scope.addTemplate = function() {
+		$scope.templates.push({
+			name : $scope.templateName
+		});
+		$scope.activeTemplate = $scope.templateName;
+		$scope.templateName = "";
+	};
 
-function templateTabContent(name) {
-	return '<div class="tab-pane" id="'
-			+ name
-			+ '"><textarea id="'
-			+ name
-			+ '-template-input" class="script-input span9" rows="10">'
-			+ '</textarea><div class="row"><div class="offset4"><button id="'
-			+ name
-			+ '-template-submit" class="btn btn-inverse disabled template-submit span2">Submit template</button><button id="'
-			+ name
-			+ '-template-delete" class="btn btn-danger template-delete span2">Delete template</button></div></div></div>';
+	$scope.$on("active", function(name) {
+		$scope.activeTemplate = name;
+	});
+
+	$scope.deleteTemplate = function() {
+		$scope.templates.splice($scope.templates.indexOf($scope.activeTemplate), 1);
+	};
+
+	$scope.submit = function() {
+		//
+	};
 }
